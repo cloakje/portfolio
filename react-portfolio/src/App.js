439bdafe44d2d23fe05ef35006d0205375978b4e
Jason <j.vanschaik@tcrmbo.nl>
2023-11-01 13:13:49 +0100

    update

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,14 +1,15 @@
-import "./App.scss";
-import { Routes, Route} from 'react-router-dom'
-import Layout from './components/Layout'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.scss';
+import Layout from './components/Layout';
 
 function App() {
-  return
-   <>
-   <Routes>
-    <Route path="/" element={<Layout />} ></Route>
-   </Routes>
-  </>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- react-portfolio/src/components/sidebar/index.js
+++ react-portfolio/src/components/sidebar/index.js
@@ -1,13 +1,14 @@
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { Link } from 'react-router-dom'
 
 const Sidebar = () => 
 <div className='nav-bar'>
-    <link className= 'logo' to='/'>
+    <Link className='logo' to='/'>
         <img src={LogoS} alt="logo" />
         <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
-    </link>
+    </Link>
     </div>
 
 export default Sidebar
